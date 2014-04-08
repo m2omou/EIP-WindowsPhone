d73d9867eeda7901280e4d19c03469d2da74ef4b
Wassim Samad <Wassim.samad@epitech.eu>
2014-04-08 13:31:13 +0800

    Ajout des vues

---
+++ NeerbyyWindowsPhone/NeerbyyWindowsPhone/CreatePost.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace NeerbyyWindowsPhone
+{
+    /// <summary>
+    /// View of the post creation
+    /// </summary>
+    public partial class CreatePost : PhoneApplicationPage
+    {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public CreatePost()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Button pressed to create the post
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CreatePostPressed(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
