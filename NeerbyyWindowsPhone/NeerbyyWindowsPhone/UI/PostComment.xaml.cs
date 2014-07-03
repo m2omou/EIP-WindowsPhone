b6bc4b75ef0724f0ba4acaed0946909b597fb5f8
Wassim Samad <Wassim.samad@epitech.eu>
2014-07-03 14:36:48 +0200

    suppression

--- NeerbyyWindowsPhone/NeerbyyWindowsPhone/UI/PostComment.xaml.cs
+++ NeerbyyWindowsPhone/NeerbyyWindowsPhone/UI/PostComment.xaml.cs
@@ -12,17 +12,40 @@ namespace NeerbyyWindowsPhone
 {
     public partial class PostComment : UserControl
     {
-        public User user;
-        public PostComment()
+        public Comment comment;
+        public PostComment(Comment comment)
         {
             InitializeComponent();
+            this.comment = comment;
+            if (comment.id == 0 || comment.user.id != WebApi.Singleton.AuthenticatedUser.id)
+                delete_button.Visibility = System.Windows.Visibility.Collapsed;
         }
 
         private void Comment_Tap(object sender, System.Windows.Input.GestureEventArgs e)
         {
 
-            ((App)Application.Current).currentUser = this.user;
+            ((App)Application.Current).currentUser = this.comment.user;
             (Application.Current.RootVisual as PhoneApplicationFrame).Navigate(new Uri("/Profile.xaml", UriKind.Relative));
+        }
+
+        private void delete_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult m = MessageBox.Show("Voulez vous vraiment supprimer votre commentaire ?", "Attention !", MessageBoxButton.OKCancel);
+            if (m == MessageBoxResult.Cancel)
+            {
+
+            }
+            else
+            {
+                WebApi.Singleton.DeleteCommentAsync((string responseMessage, Result result) =>
+                {
+                    this.Visibility = System.Windows.Visibility.Collapsed;
+                }, (String responseMessage, Exception exception) =>
+                {
+                    ErrorDisplayer error = new ErrorDisplayer();
+                }, comment);
+            } 
+
         }
 
 
